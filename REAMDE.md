e68fedd4645ee9605ff36f01e6b372174eb1ee69
Kyle Gorman <kylebgorman@gmail.com>
2016-07-04 20:26:32 -0400

    First commit.

---
+++ REAMDE.md
@@ -0,0 +1,4 @@
+Verb matching example
+=====================
+
+match.R is an example of attempting to match regular and irregular English verbs on frequency and several related attributes. There are more than an order of magnitude more regular verbs in the data set, and irregulars tend to be significantly higher frequency, so this is somewhat challenging.
---
+++ match.R
@@ -0,0 +1,58 @@
+#!/usr/bin/env Rscript
+# Matches regular and irregular verbs on:
+# 
+# * log wordform frequency
+# * log lemma frequency
+# * suffix conditional probability
+# * OLD20
+# * squared orthographic word length
+# * number of syllables
+#
+# Thanks to Constantine Lignos for help with the ELP and SUBTLEXus data.
+
+library(ldamatch)
+library(plyr)
+
+ELP_WORDS_MERGED <- "elp_words_merged.csv"
+ENGLISH_IRREGULARS <- "english_irregulars.csv"
+
+# Creates vector of case-folded irregular forms.
+get_irregulars <- function() {
+  d <- read.csv(ENGLISH_IRREGULARS)
+  factor(casefold(unique(with(d, c(as.character(VBD), as.character(VBN))))))
+}
+
+# Creates dataframe with all ELP variables.
+get_elp <- function() {
+  irregulars <- get_irregulars()
+  d <- subset(read.csv(ELP_WORDS_MERGED))
+  # Possibly a past tense verb.
+  d <- subset(d, suffix == "ed" | word %in% irregulars)
+  d <- with(d, data.frame(word=casefold(word),
+                          root=casefold(root),
+                          irreg=as.factor(ifelse(word %in% irregulars,
+                                                 "irregular", "regular")),
+                          sbtlx.freq,
+                          sbtlx.basefreq,
+                          sbtlx.pformbase,
+                          OLD,
+                          length.squared=length * length, 
+                          n.syll))
+  d[complete.cases(d), ]
+}
+
+# Main.
+d <- get_elp()
+# Small set of covariates, for testing.
+covariates <- with(d, cbind(sbtlx.freq, OLD))
+# Big set of covariates, what I really want to match on.
+#covariates <- with(d, cbind(sbtlx.freq, sbtlx.basefreq, sbtlx.pformbase,
+#                            OLD, length.squared, n.syll))
+
+# Match on whatever method works, favoring preserving irregulars.
+METHODS <- c("heuristic2", "heuristic3", "heuristic4", "random")
+for (method in METHODS) {
+  is.in <- match_groups(d$irreg, covariates, halting_test=t_halt,
+                        props="irregular", method=method)
+  print(table(is.in))
+}
