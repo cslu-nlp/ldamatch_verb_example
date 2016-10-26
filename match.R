134a5e32cce00b1439212cca454ee8f7e8efc496
Geza Kiss <kiss@ohsu.edu>
2016-10-26 14:40:09 +0200

    finds solution with heuristic1 if using new ldamatch version (1.0.1.3 or greater)

--- match.R
+++ match.R
@@ -1,8 +1,8 @@
 #!/usr/bin/env Rscript
 # Kyle Gorman <kylebgorman@gmail.com>
-# 
+#
 # Matches regular and irregular verbs on:
-# 
+#
 # * log wordform frequency
 # * log lemma frequency
 # * suffix conditional probability
@@ -31,15 +31,15 @@ get_elp <- function() {
   d <- subset(read.csv(ELP_WORDS_MERGED))
   # Possibly a past tense verb.
   d <- subset(d, suffix == "ed" | word %in% irregulars)
-  d <- with(d, data.frame(word=casefold(word),
-                          root=casefold(root),
-                          regularity=as.factor(ifelse(word %in% irregulars,
-                                                      "irregular", "regular")),
+  d <- with(d, data.frame(word = casefold(word),
+                          root = casefold(root),
+                          regularity = as.factor(ifelse(word %in% irregulars,
+                                                        "irregular", "regular")),
                           sbtlx.freq,
                           sbtlx.basefreq,
                           sbtlx.pformbase,
                           OLD,
-                          length.squared=length * length, 
+                          length.squared = length * length,
                           n.syll))
   d[complete.cases(d), ]
 }
@@ -53,9 +53,20 @@ covariates <- with(d, sbtlx.freq)
 #                            OLD, length.squared, n.syll))
 # Matches on whatever method works, favoring preserving irregulars (of which
 # there are many fewer).
-METHODS <- c("heuristic2")
+METHODS <- c("heuristic1")
 for (method in METHODS) {
-  match_groups(d$regularity, covariates, halting_test=t_halt,
-               props=c(irregular=.5, regular=.5), max_removed=c(irregular=0),
-               method=method)
+  l = match_groups(d$regularity, covariates, halting_test = t_halt,
+               method = method,
+               print_info = TRUE, use_test = TRUE,
+               max_removed = c(irregular = 0) # equivalent to: props = 'irregular'
+               )
 }
+
+# Why heuristic2, 3, and 4 are slow: These algorithms calculate
+# the p-value with all combinations of lookahead subjects removed
+# (where lookahead is 1 for heuristic2, and 2 by default for heuristic3 and 4).
+# Then remove one subject that seems to give the biggest improvement in p-value.
+# Then they re-calculate all p-values.
+# Perhaps they could be speeded up by removing multiple subjects at a time
+# when there are a large number of subjects and the p-values are very low below
+# the threshold.
